b5e85dd5986f2cfbf19bcf5bb86c2ea1b49aa873
justin <justin0979@gmail.com>
2019-07-24 16:25:47 -0500

    Updates

---
+++ precache-manifest.fd4ad9a6fcf39985f4599d969b605747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1794ca931152ac2fc75bd67580f7bf2d",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "da765c1e3ad40359067f",
+    "url": "/RoboFriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "8818de161e76aba276c9",
+    "url": "/RoboFriends/static/css/main.e96588a8.chunk.css"
+  },
+  {
+    "revision": "da765c1e3ad40359067f",
+    "url": "/RoboFriends/static/js/2.9ff0d512.chunk.js"
+  },
+  {
+    "revision": "8818de161e76aba276c9",
+    "url": "/RoboFriends/static/js/main.02c7245d.chunk.js"
+  },
+  {
+    "revision": "d48d68f29921c5c588da",
+    "url": "/RoboFriends/static/js/runtime~main.19bee101.js"
+  }
+]);
